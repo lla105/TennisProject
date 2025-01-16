ab831abe92f5735723793e139acbd5844c46a23e
lla105 <lla105@sfu.ca>
2025-01-16 12:11:26 -0800

    ..

--- src/process.py
+++ src/process.py
@@ -418,7 +418,7 @@ def main():
     videoname = 'temp11'
     # videoname = 'test16_4k'
     videoname = 'small'
-    videoname = '1118 (1)'
+    # videoname = '1118 (1)'
 
     video_process(video_path=f'../videos/{videoname}.MP4', show_video=True, stickman=True, stickman_box=False, smoothing=True,
                   court=False, top_view=True, videoname=videoname)
