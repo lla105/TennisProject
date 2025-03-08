5f240f4c884f498517500edaace69ecbd33788a1
lla105 <lla105@sfu.ca>
2025-03-08 13:20:49 -0800

    ..

--- src/test.py
+++ src/test.py
@@ -4,6 +4,7 @@ import os
 # Load the coordinates to inspect
 coordinates_file = os.path.join('output', f"ball_positions.npy")
 
+
 xy_coordinates = np.load( coordinates_file, allow_pickle=True)
 print("Loaded coordinates:", xy_coordinates)
 print("Shape of loaded coordinates:", xy_coordinates.shape)
